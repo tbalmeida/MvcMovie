940260f49dcd9d2ab2b25107bedafd42020b2c6b
Thiago Almeida <tbalmeida@gmail.com>
2020-11-01 16:38:56 -0700

    Added model and initial migration

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MvcMovie.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +21,9 @@ namespace MvcMovie
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllersWithViews();
+
+      services.AddDbContext<MvcMovieContext>(options =>
+          options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
